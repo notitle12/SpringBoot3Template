c4a891628b195fa27ce0e251c36fe0f2ff29d49d
notitle12 <notitle1215@gmail.com>
2025-03-03 01:51:39 +0900

    feat: validation 의존성 및 dto에 벨리데이션 추가

--- src/main/java/com/example/springboot3template/auth/application/dto/req/SignUpReq.java
+++ src/main/java/com/example/springboot3template/auth/application/dto/req/SignUpReq.java
@@ -1,13 +1,22 @@
 package com.example.springboot3template.auth.application.dto.req;
 
 import com.example.springboot3template.auth.domain.entity.UserRoleEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class SignUpReq {
 
+    @NotBlank(message = "사용자 이름(username)은 필수 입력값입니다.")
+    @Size(min = 4, max = 10, message = "사용자 이름(username)은 4자리 이상 10자리 이하로 입력해야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "사용자 이름(username)은 영어와 숫자만 입력 가능합니다.")
     private String username;
 
+    @NotBlank(message = "비밀번호(password)는 필수 입력값입니다.")
+    @Size(min = 4, max = 10, message = "비밀번호(password)는 4자리 이상 10자리 이하로 입력해야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "비밀번호(password)는 영어와 숫자만 입력 가능합니다.")
     private String password;
 
     private UserRoleEnum role;
