7d27da21279d392eedd2d59fa73edbe9225802c5
notitle12 <notitle1215@gmail.com>
2025-03-03 22:57:33 +0900

    refactor: 전역예외처리 필드 하나 추가

--- src/main/java/com/example/springboot3template/common/globalException/CustomException.java
+++ src/main/java/com/example/springboot3template/common/globalException/CustomException.java
@@ -8,11 +8,13 @@ public class CustomException extends RuntimeException {
     private final int code;
     private final String message;
     private final HttpStatus httpStatus;
+    private final int status;
 
     public CustomException(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.code = errorCode.getCode();
         this.message = errorCode.getMessage();
         this.httpStatus = errorCode.getHttpStatus();
+        this.status = errorCode.getHttpStatus().value();
     }
 }
--- src/main/java/com/example/springboot3template/common/globalException/GlobalExceptionHandler.java
+++ src/main/java/com/example/springboot3template/common/globalException/GlobalExceptionHandler.java
@@ -17,14 +17,14 @@ public class GlobalExceptionHandler {
     // 특정 예외 처리 (IllegalArgumentException)
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorRes> handleIllegalArgumentException(IllegalArgumentException ex) {
-        ErrorRes response = new ErrorRes(4000, ex.getMessage(), HttpStatus.BAD_REQUEST, LocalDateTime.now());
+        ErrorRes response = new ErrorRes(4000, ex.getMessage(), HttpStatus.BAD_REQUEST, HttpStatus.BAD_REQUEST.value(), LocalDateTime.now());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
     // 일반적인 예외 처리 (NullPointerException 등 포함)
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorRes> handleGeneralException(Exception ex) {
-        ErrorRes response = new ErrorRes(9999, ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, LocalDateTime.now());
+        ErrorRes response = new ErrorRes(9999, ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR.value(), LocalDateTime.now());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 }
